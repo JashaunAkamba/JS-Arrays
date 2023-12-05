5243360ca8fba8a9fc57c7f33b3c00ea6d7d3b9f
Jashaun Akamba <jashaunakamba12@gmail.com>
2023-12-05 17:11:01 -0500

    main

---
+++ script.js
@@ -0,0 +1,16 @@
+let x = 5;
+
+console.log(x)
+
+let a = [2,21,21,34,5,434,35]
+
+console.log(a)
+console.log(a.length)
+
+a.push(100)
+console.log(a[100])
+
+let b = ["car", "truck","bike"];
+console.log (b)
+
+console.log(b[1])
