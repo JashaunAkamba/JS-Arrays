fe118b25f85fc1f89883fa25977573a831029bd6
Jashaun Akamba <jashaunakamba12@gmail.com>
2023-12-07 16:53:27 -0500

    grades updated

--- script.js
+++ script.js
@@ -1,16 +1,38 @@
-let x = 5;
+// let x = 5;
 
-console.log(x)
+// console.log(x)
 
-let a = [2,21,21,34,5,434,35]
+// let a = [2,21,21,34,5,434,35]
 
-console.log(a)
-console.log(a.length)
+// console.log(a)
+// console.log(a.length)
 
-a.push(100)
-console.log(a[100])
+// a.push(100)
+// console.log(a[100])
 
-let b = ["car", "truck","bike"];
-console.log (b)
+// let b = ["car", "truck","bike"];
+// console.log (b)
 
-console.log(b[1])
+// console.log(b)
+
+// console.log
+
+let rectangle = {width:25, height:50,area:()=>{return 25*50}}
+function area () {
+    return 25*50 
+}
+
+let person1={name:'Jeff',age:46}
+let person2={name:'Jashaun',age:15}
+let person3={name:'Derron',age:15}
+
+
+let people= [person1,person2,person3]
+
+console.log(person1.age+person2.age+person3.age)
+
+let sum=0;
+for (let i=0;i<people.length; i++) {
+    sum+=people[i].age;
+}
+console.log(sum/people.length);
